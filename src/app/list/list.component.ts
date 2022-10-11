ec597abde52ca3453877c2d6f7628609b4d249fb
Sivalatha800 <rachamallusivalatha@gmail.com>
2022-10-11 19:53:56 +0530

    Ending Step

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,6 +11,9 @@ export class ListComponent implements OnInit {
   throttle = 1000;
   scrollDistance = 1;
   scrollUpDistance = 1;
+  filterString: any;
+  checkBoxData: any = [];
+  showClear: boolean = false;
   constructor(
     public activatedRrouter: ActivatedRoute,
     public listService: ListService,
@@ -22,6 +25,7 @@ export class ListComponent implements OnInit {
     this.getList();
   }
 
+  // Getting Params
   getParams() {
     this.activatedRrouter.params.subscribe((paramValues: Params) => {
       this.listService.parem1 = paramValues['param1'];
@@ -34,18 +38,24 @@ export class ListComponent implements OnInit {
 
   //intial call api call with and with out query params
   getList() {
-    // console.log(this.listService.queryParamValues);
     // converting params to string text
     const filterString = Object.keys(this.listService.queryParamValues)
       .map((data) => `${data}=${this.listService.queryParamValues[data]}`)
       .join('|');
-
+    //clear option show or hide
+    const paramsLength = Object.keys(this.listService.queryParamValues);
+    if (paramsLength.length != 0) {
+      this.showClear = true;
+    } else {
+      this.showClear = false;
+    }
+    // setting query params before api call
     this.listService.parametersObj.f = filterString;
-
-    this.listService.parametersObj.pno = this.listService.pageNo;
+    //setting params before api call
+    this.listService.parametersObj.pno = 1;
     this.listService.parametersObj.ps = this.listService.pageSize;
     this.listService.parametersObj.by = 'popular';
-
+    // api call
     this.listService.getApiForAll().subscribe((res) => {
       this.listService.productsList = res.Data.ProductsDetails;
       this.listService.totalRecords = res.Data.TotalRecords;
@@ -85,43 +95,58 @@ export class ListComponent implements OnInit {
     });
   }
 
-  checkBoxData: any = [];
-  checkBoxData2: any = [];
+  //checkbox functionality
   checkBoxFun(event: any, keyName: any) {
-    let { checked, value } = event.target;
+    this.showClear = true;
     let url: any;
-    if (checked == true) {
-      if (this.checkBoxData2.length == 0) {
-        this.checkBoxData2.push({ [keyName]: value });
-      } else {
-        console.log('Multy Select');
-      }
-    } else {
-      this.checkBoxData2.forEach((check: any, index: any) => {
-        let val = Object.keys(check);
-        val = val.filter((key) => {
-          if (check[key] == value) {
-          }
-        });
-      });
-    }
-
     //passing single catagery
     let checkedVal = Object.keys(this.checkBoxData);
+
     checkedVal = checkedVal.filter((key) => this.checkBoxData[key] == true);
+    console.log(checkedVal);
     var checkedJoin = checkedVal.join(':');
 
     if (checkedJoin != '') {
       url = {
         [keyName]: checkedJoin,
       };
+
+      this.route.navigate([this.listService.parem1, this.listService.parem2], {
+        queryParams: url,
+        queryParamsHandling: 'merge',
+      });
+    } else {
+      this.route.navigate([this.listService.parem1, this.listService.parem2], {
+        queryParams: url,
+      });
     }
-    this.route.navigate([this.listService.parem1, this.listService.parem2], {
-      queryParams: url,
-    });
+
     this.getParams();
     setTimeout(() => {
       this.getList();
     }, 300);
+  }
+
+  sampleData: any[] = [];
+  data: any = {
+    value: 'value',
+    checked: 'checked',
+  };
+
+  //clear all filters
+  onClearFilters() {
+    this.checkBoxData = [];
+    this.showClear = false;
+    this.route.navigate([this.listService.parem1, this.listService.parem2]);
+    this.listService.parametersObj.pno = 1;
+    this.listService.parametersObj.ps = this.listService.pageSize;
+    this.listService.parametersObj.by = 'popular';
+    this.listService.parametersObj.f = '';
+
+    this.listService.getApiForAll().subscribe((res) => {
+      this.listService.productsList = res.Data.ProductsDetails;
+      this.listService.totalRecords = res.Data.TotalRecords;
+      this.listService.filtersList = res.Data.Facets;
+    });
   }
 }
